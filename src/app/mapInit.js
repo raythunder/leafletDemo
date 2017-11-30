9a70e1d94cbc7bc56c68a0cc93824b948a4a4a3d
caizb <caizhibin@cleartv.cn>
2017-11-30 16:41:08 +0800

    demo注释单图层加载

--- src/app/mapInit.js
+++ src/app/mapInit.js
@@ -5,22 +5,22 @@ var normalm = L.tileLayer.chinaProvider('GaoDe.Normal.Map', {
 });
 var normal = L.layerGroup([normalm])
 
-var layer = {
-    imageUrl: 'src/images/map2.png',
-    imageBounds: [
-        [28.710463073923, 120.621643066406],
-        [28.668901074144, 120.590744018555]
-    ]
-}
-var imageLayer = L.imageOverlay(layer.imageUrl, layer.imageBounds, {
-    zIndex: 0,
-    className: ' base-map'
-})
+// var layer = {
+//     imageUrl: 'src/images/map2.png',
+//     imageBounds: [
+//         [28.710463073923, 120.621643066406],
+//         [28.668901074144, 120.590744018555]
+//     ]
+// }
+// var imageLayer = L.imageOverlay(layer.imageUrl, layer.imageBounds, {
+//     zIndex: 0,
+//     className: ' base-map'
+// })
 
 // var map = L.map('map').setView([35.10418, -106.62987],10);
 var map = L.map('map', {
     center: [28.6896820740335, 120.6061935424805],
     zoom: 12,
-    layers: [normal,imageLayer],  // 图层
+    layers: [normal],  // 图层
     zoomControl: false
 });
