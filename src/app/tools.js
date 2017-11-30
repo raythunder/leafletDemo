bcbb0001bcb3484a999d0712ceb420d18efffe09
caizb <caizhibin@cleartv.cn>
2017-11-30 16:35:21 +0800

    图层组合demo

--- src/app/tools.js
+++ src/app/tools.js
@@ -1,5 +1,5 @@
 map.on('click',function(e){
-    console.log(e.latlng)
+    console.log([e.latlng.lat,e.latlng.lng])
     L.marker([e.latlng.lat,e.latlng.lng],{
         title:"MyPoint",alt:"The Big I",draggable:true
     }).addTo(map)
